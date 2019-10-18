bf48f83d8efa53c76406f12fee96c6a9dd646650
Tim Gottman <timothy.gottman@student.swic.edu>
2019-10-17 22:15:31 -0500

    Rework of the underling code in the script. Added minor CSS to html. Added text to the home page describing services.

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@
  */
 //Cost Each fruit based on QTY ordered.
 //Idealy there is a way to dynamic track the Qty from the form and pull the variable in, but this is a more simple solution than multiple layers of setting the value of both cost and qty.
+//Found an ideal concise method to avoid code repeat.
 const fruitCost = {
   apple(a) {
     if (a < 10) {
@@ -31,6 +32,32 @@ const fruitCost = {
     if (a < 10) {
       return a * 2.5;
     } else return a * 2;
+  },
+  fruitSum(a, o, b, p, m) {
+    return (
+      Number(fruitCost.apple(a)) +
+      Number(fruitCost.orange(o)) +
+      Number(fruitCost.banana(b)) +
+      Number(fruitCost.pear(p)) +
+      Number(fruitCost.melon(m))
+    );
+  }
+};
+const deliveryCost = {
+  weekly(a, o, b, p, m) {
+    return (
+      Number(fruitCost.fruitSum(a, o, b, p, m)) +
+      Number(fruitCost.fruitSum(a, o, b, p, m)) * 0.1
+    );
+  },
+  daily(a, o, b, p, m) {
+    return (
+      Number(fruitCost.fruitSum(a, o, b, p, m)) +
+      Number(fruitCost.fruitSum(a, o, b, p, m) * 0.15)
+    );
+  },
+  monthly(a, o, b, p, m) {
+    return Number(fruitCost.fruitSum(a, o, b, p, m));
   }
 };
 
@@ -44,36 +71,47 @@ const output = document.querySelector("output");
 
 output.form.addEventListener("submit", function(event) {
   event.preventDefault();
-  let delivery = [select.value];
-  console.log(delivery);
-  if (delivery == "D") {
-    let cost =
-      Number(fruitCost.apple(apple.value)) +
-      Number(fruitCost.orange(orange.value)) +
-      Number(fruitCost.banana(banana.value)) +
-      Number(fruitCost.pear(pear.value)) +
-      Number(fruitCost.melon(melon.value));
-    console.log(cost);
-    output.textContent = cost + Number(cost * 0.15);
-  } else if (delivery == "W") {
-    let cost =
-      Number(fruitCost.apple(apple.value)) +
-      Number(fruitCost.orange(orange.value)) +
-      Number(fruitCost.banana(banana.value)) +
-      Number(fruitCost.pear(pear.value)) +
-      Number(fruitCost.melon(melon.value));
-    console.log(cost);
+  output.textContent = deliveryCost[select.value](
+    apple.value,
+    orange.value,
+    banana.value,
+    pear.value,
+    melon.value
+  ).toFixed(2);
 
-    output.textContent = cost + Number(cost * 0.1);
-  } else {
-    let cost =
-      Number(fruitCost.apple(apple.value)) +
-      Number(fruitCost.orange(orange.value)) +
-      Number(fruitCost.banana(banana.value)) +
-      Number(fruitCost.pear(pear.value)) +
-      Number(fruitCost.melon(melon.value));
-    console.log(cost);
+  // OLD CODE TO CONFIRM WORKING HTML
+  //
+  //  output.textContent = fruitCost.fruitSum();
+  // let delivery = [select.value];
+  // console.log(delivery);
+  // if (delivery == "D") {
+  //   let cost =
+  //     Number(fruitCost.apple(apple.value)) +
+  //     Number(fruitCost.orange(orange.value)) +
+  //     Number(fruitCost.banana(banana.value)) +
+  //     Number(fruitCost.pear(pear.value)) +
+  //     Number(fruitCost.melon(melon.value));
+  //   console.log(cost);
+  //   output.textContent = cost + Number(cost * 0.15);
+  // } else if (delivery == "W") {
+  //   let cost =
+  //     Number(fruitCost.apple(apple.value)) +
+  //     Number(fruitCost.orange(orange.value)) +
+  //     Number(fruitCost.banana(banana.value)) +
+  //     Number(fruitCost.pear(pear.value)) +
+  //     Number(fruitCost.melon(melon.value));
+  //   console.log(cost);
 
-    output.textContent = cost + Number(cost * 0.05);
-  }
+  //   output.textContent = cost + Number(cost * 0.1);
+  // } else {
+  //   let cost =
+  //     Number(fruitCost.apple(apple.value)) +
+  //     Number(fruitCost.orange(orange.value)) +
+  //     Number(fruitCost.banana(banana.value)) +
+  //     Number(fruitCost.pear(pear.value)) +
+  //     Number(fruitCost.melon(melon.value));
+  //   console.log(cost);
+
+  //   output.textContent = cost + Number(cost * 0.05);
+  // }
 });
