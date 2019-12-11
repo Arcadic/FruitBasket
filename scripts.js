9aa7533cde3a199cb4386a9318db51559a051b0a
Tim Gottman <timothy.gottman@student.swic.edu>
2019-12-10 22:25:55 -0600

    Final correction before push. Commented out the code that displays the whole array when adding users.

--- scripts.js
+++ scripts.js
@@ -54,28 +54,28 @@ function add_user() {
   reg_name.value = reg_street.value = reg_city.value = reg_zipcode.value = "";
 
   //create new div and append name, street, city and zipcode
-  var old_records_div = document.getElementById("old-records");
-  var new_record_div = document.createElement("DIV");
-  var new_record_name_span = document.createElement("SPAN");
-  var new_record_street_span = document.createElement("SPAN");
-  var new_record_city_span = document.createElement("SPAN");
-  var new_record_zipcode_span = document.createElement("SPAN");
-  var new_record_deliveryCost_span = document.createElement("SPAN");
+  // var old_records_div = document.getElementById("old-records");
+  // var new_record_div = document.createElement("DIV");
+  // var new_record_name_span = document.createElement("SPAN");
+  // var new_record_street_span = document.createElement("SPAN");
+  // var new_record_city_span = document.createElement("SPAN");
+  // var new_record_zipcode_span = document.createElement("SPAN");
+  // var new_record_deliveryCost_span = document.createElement("SPAN");
 
-  new_record_name_span.innerHTML = "Name: " + temp_detail.name;
-  new_record_street_span.innerHTML = "\nStreet: " + temp_detail.street;
-  new_record_city_span.innerHTML = "\nCity: " + temp_detail.city;
-  new_record_zipcode_span.innerHTML = "\nZip Code: " + temp_detail.zipcode;
-  new_record_deliveryCost_span.innerHTML =
-    "\nTotal Cost: $" + temp_detail.totalCost;
+  // new_record_name_span.innerHTML = "Name: " + temp_detail.name;
+  // new_record_street_span.innerHTML = "\nStreet: " + temp_detail.street;
+  // new_record_city_span.innerHTML = "\nCity: " + temp_detail.city;
+  // new_record_zipcode_span.innerHTML = "\nZip Code: " + temp_detail.zipcode;
+  // new_record_deliveryCost_span.innerHTML =
+  //   "\nTotal Cost: $" + temp_detail.totalCost;
 
-  new_record_div.appendChild(new_record_name_span);
-  new_record_div.appendChild(new_record_street_span);
-  new_record_div.appendChild(new_record_city_span);
-  new_record_div.appendChild(new_record_zipcode_span);
-  new_record_div.appendChild(new_record_deliveryCost_span);
+  // new_record_div.appendChild(new_record_name_span);
+  // new_record_div.appendChild(new_record_street_span);
+  // new_record_div.appendChild(new_record_city_span);
+  // new_record_div.appendChild(new_record_zipcode_span);
+  // new_record_div.appendChild(new_record_deliveryCost_span);
 
-  old_records_div.appendChild(new_record_div);
+  // old_records_div.appendChild(new_record_div);
 
   //saving record in array and printing the array
   allRegistered.push(temp_detail);
